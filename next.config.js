65a5da27a587bdf79da5ff30722fea78f5adaf41
crown0205 <crown0205@gmail.com>
2024-02-23 19:51:33 +0900

    fix : "domains"는 Next.js 14부터 더 이상 사용되지 않아, "remotePatterns"로 변경

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     // NOTE: 이미지 CDN 도메인 추가
-    domains: ["cdn.heyseller.kr"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.heyseller.kr",
+      },
+    ],
   },
   // NOTE: SVG 파일을 컴포넌트로 사용하기 위한 설정
   webpack: (config) => {
